5dc900761099eaac74fdfaf59b2a6e73928fe1e6
u1dm <127678638+u1dm@users.noreply.github.com>
2024-01-24 13:45:47 +0200

    Create rickroll.sh

---
+++ rickroll.sh
@@ -0,0 +1,4 @@
+echo "долбоеб я щас мог те всё нахуй снести но я тебя люблю"
+echo "щас будет рикролл"
+sleep 5
+curl -s -L https://raw.githubusercontent.com/keroserene/rickrollrc/master/roll.sh | bash
